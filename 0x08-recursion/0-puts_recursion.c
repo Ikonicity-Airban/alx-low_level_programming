d35675abd3feaa20f36cfc18895c3a65560d4766
root <ikonicityairban@gmail.com>
2023-06-07 17:43:59 -0700

    0-puts_recursion.c betty approved

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _puts_recursion - A function that prints a string
+ * adds a new line
+ * @c: A string to be printed.
+ */
+
+void _puts_recursion(char *c)
+{
+	if (*c)
+	{
+		_putchar(*c++);
+		_puts_recursion(c);
+	}
+	_putchar('\n');
+}
+
