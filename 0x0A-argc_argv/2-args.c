21272917b62940bb6c83a9cfd0d3ac926b5d03ea
root <ikonicityairban@gmail.com>
2023-06-17 21:07:33 -0700

    printing all arguments of main
    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: A program that prints th name of the user
+ * passed in from the command line as argument
+ * @argv: A vector holding the command line arguments.
+ * @argc: the number of elements in argv
+ * Return: Always return zero.
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	while (++(*argv) = NULL)
+	{
+		i = 0;
+		while (*argv[i] != NULL)
+		{
+			_putchar(*argv[i]++);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
