c43265b6efdd518af8ead25b79583b43ccce464e
root <ikonicityairban@gmail.com>
2023-06-17 20:54:00 -0700

    Amount of argument passed printed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: A program that prints th name of the user
+ * passed in from the command line as argument
+ * @argv: A vector holding the command line arguments.
+ * @argc: the number of elements in argv
+ * Return: Always return zero.
+ */
+
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	_putchar(argc + '0');
+	_putchar('/');
+	return (0);
+}
