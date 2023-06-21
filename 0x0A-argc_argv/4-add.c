5192c0e0c7fcbf9365b76e5ef6ad1712670b02e5
root <ikonicityairban@gmail.com>
2023-06-21 02:41:28 -0700

    4-add c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,25 @@
 #include"main.h"
 
 /**
+ * isNum - check if string array is num
+ * @num: string to check
+ * Return: 0 if it's a number
+ *         1 if it's not a number
+*/
+
+int isNum(char num[])
+{
+	int i, l = strlen(num);
+
+	for (i = 0; i < l; i++)
+	{
+		if (!isdigit(num[i]))
+			return (1);
+	}
+	return (0);
+}
+
+/**
  * main - Entry point.
  * Description: A program that adds numbers passed
  * as argument.
@@ -13,17 +32,22 @@
 
 int main(int argc, char **argv)
 {
-	int sum, i = 1;
-
-	while (argv[i] < argc)
+	int sum, i;
+	
+	/* checks if no argument was passed to the program */
+	if (argc < 2)
 	{
-		if (!isdigit(atoi(argv[i])))
+		putchar(0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isNum(argv[i]) == 1)
 		{
 			printf("Error\n");
 			return (1);
 		}
 		sum += atoi(argv[i]);
-		i++;
 	}
 	printf("%d\n", sum);
 	return (0);
